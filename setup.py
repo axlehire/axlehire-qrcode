b23b8ac4285e2435554ec389061fc52a2237fc63
longd3 <izzy3ds@gmail.com>
2018-02-02 16:30:08 -0800

    Add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Setup tool
+"""
+
+
+from setuptools import setup
+
+
+setup(name='myqr',
+      version='1.0',
+      description='',
+      url='https://github.com/axlehire/axlehire-qrcode',
+      author='Long Tran',
+      author_email='long@axlehire.com',
+      packages=['MyQR'],
+      include_package_data=True,
+      install_requires=['imageio', 'numpy', 'Pillow'],
+      zip_safe=False)
